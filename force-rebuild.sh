8bc63951ec79a0ab66ca099c8d87ecb28dee92ec
mwerlen <maxime@werlen.fr>
2018-10-31 13:37:37 +0100

    force xz to compress even if archive is already present

--- force-rebuild.sh
+++ force-rebuild.sh
@@ -2,5 +2,5 @@ sudo debootstrap sid rootfs http://ftp.debian.org/debian
 sudo tar -C rootfs -cf rootfs.tar .
 sudo chown mwerlen:mwerlen rootfs.tar
 sudo rm -rf rootfs
-xz rootfs.tar
+xz -f rootfs.tar
 docker build --no-cache  -t mwerlen/debian-sid .
